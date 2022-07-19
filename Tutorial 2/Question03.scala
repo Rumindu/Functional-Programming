8b11b6397ed361cb6c68d8f39961091f3b7189a9
Rumindu <108813657+Rumindu@users.noreply.github.com>
2022-07-19 06:58:50 +0530

    Add files via upload

---
+++ Tutorial 2/Question03.scala
@@ -0,0 +1,19 @@
+object q3 extends App {
+    
+    var a = 2
+    var b = 3
+    var c = 4
+    var d = 5
+    var k = 4.3f
+    var g = 4.0f
+
+    println({b-=1;b*a+c*d});
+    d-=1;
+    println(a);
+    a+=1;
+    println(-2*(g-k)+c);
+    println(c);
+    c+=1;
+    println({c+=1;c*a});
+    a+=1;
+}
